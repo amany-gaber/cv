a64d3b0c11e4e7d0e51346e042e1bdc08d1d6733
Amany Gaber <116579137+amany-gaber@users.noreply.github.com>
2025-05-14 22:36:12 +0300

    Update result.py

--- result.py
+++ result.py
@@ -1,13 +1,21 @@
-fastapi==0.115.0
-uvicorn==0.30.6
-torch==2.4.1
-torchvision==0.19.1
-numpy==1.26.4
-opencv-python==4.10.0.84
-python-multipart==0.0.9
-librosa==0.9.2
-scipy==1.13.1
-numba==0.60.0
-gunicorn==20.1.0
-requests
-lipsync
+/opt/nvidia/nvidia_entrypoint.sh: line 67: exec: gunicorn: not found
+yaz@gpu:~/tak/GP/AVATAR$ ls
+API.py  docker-compose.yml  inference.py  lipsync_test.py  requirements.txt  videos   Wav2Lip_inference_patched.py
+cache   dockerfile          lipsync.py    __pycache__      source            Wav2Lip  weights
+yaz@gpu:~/tak/GP/AVATAR$ 
+
+
+
+
+
+
+.env
+CACHE_DIR=/app/cache
+STATIC_VIDEO=/app/source/final.mp4
+OUTPUT_VIDEO=/app/result.mp4
+UPLOAD_AUDIO_DIR=/app/source
+VIDEOS_DIR=/app/videos
+CHECKPOINT_PATH=/app/weights/wav2lip.pth
+FFMPEG_LOGLEVEL=quiet
+API_PORT=4097
+EXPOSE_PORT=1236
